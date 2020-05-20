608b9a26752a849d061043eb90736cb5af8a2cf0
xiaoqingrong <2724226569@qq.com>
2020-05-20 10:25:10 +0800

    一键启动完成

--- src/node/app.js
+++ src/node/app.js
@@ -4,7 +4,7 @@ var app = express();
 // 设置跨域
 
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8888");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1')
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports={
     devServer: {
+        port: 8888,
         overlay: {
             warnings: false,
             errors: false
